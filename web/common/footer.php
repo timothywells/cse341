021c943513afed73215df4405cf1661a33b16bac
Timothy Wells <mr.timothywells@gmail.com>
2020-09-27 16:05:48 -0700

    20.09.27.01

---
+++ web/common/footer.php
@@ -0,0 +1,1 @@
+<p id="copyright">&copy; <script>document.write(new Date().getFullYear())</script> Timothy Wells | WDD 330 - Back End Development II | All rights reserved</p>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home Page | CSE 341 - Back End Development II</title>
+        <meta name="description" content="WDD 330 - Front End Development Home Page designed and developed by Tim Wells">
 
-  phpinfo();
+        <?php include $_SERVER['DOCUMENT_ROOT'].'/common/head.php'; ?>
 
-?>
+    </head>
+    <header>
+        <h1>CSE 341 - Back End Web Development II</h1>
+    </header>
+<div class="row">
+    <nav>
+        <ul class="wk_nav">
+            
+        </ul>
+    </nav> 
+
+    <main>
+        <h1>Introduction</h1>
+            <p></p>
+            <p></p>
+    </main>
+</div>
+    <footer>
+        <?php include $_SERVER['DOCUMENT_ROOT'].'/common/footer.php'; ?>
+    </footer>
+    <script src="/js/javascript.js"></script>
+</html>
