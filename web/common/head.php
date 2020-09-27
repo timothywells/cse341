db4770988e7524eaa74db188f09543ba729ad565
Timothy Wells <mr.timothywells@gmail.com>
2020-09-27 16:18:56 -0700

    20.09.27.02
    Linking

--- web/common/head.php
+++ web/common/head.php
@@ -1,9 +1,9 @@
 <meta charset="utf-8">
 <meta name="author" content="Timothy Wells">
 <meta name="keywords" content="HTML, CSS, JavaScript, WDD 330, Front End Development">
-<link rel="stylesheet" href="/cse241/web/css/stylesheet.css">
+<link rel="stylesheet" href="/css/stylesheet.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="apple-touch-icon" sizes="180x180" href="/cse241/web/favicon/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/cse241/web/favicon/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/cse241/web/favicon/favicon-16x16.png">
-<link rel="manifest" href="/cse241/web/favicon/site.webmanifest">
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+<link rel="manifest" href="/favicon/site.webmanifest">
