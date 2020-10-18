2946415cb1ac119c1c101e3302b0440e4f558eeb
Timothy Wells <mr.timothywells@gmail.com>
2020-10-18 15:58:52 -0700

    20.10.18.05
    SIte Fixes

--- web/common/head.php
+++ web/common/head.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="author" content="Timothy Wells">
 <meta name="keywords" content="HTML, CSS, JavaScript, WDD 330, Front End Development">
-<link rel="stylesheet" href="web/css/stylesheet.css">
+<link rel="stylesheet" href="/css/stylesheet.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
