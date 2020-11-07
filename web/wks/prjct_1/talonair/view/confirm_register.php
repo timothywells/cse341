f013544ce61d87eb74b495c05cb710395ecad2cd
Timothy Wells <mr.timothywells@gmail.com>
2020-11-07 16:08:42 -0700

    20.11.07.23

--- web/wks/prjct_1/talonair/view/confirm_register.php
+++ web/wks/prjct_1/talonair/view/confirm_register.php
@@ -16,11 +16,11 @@
 	<div class="formatted_view">
         <!-- Div needed to format center and pic -->
 		<h3>Thank You for Registering</h3>
-
-		<?php
-		if(isset($reg_meesage)){echo $reg_message;};
-		?>
-
+		<p>
+			<?php
+			if(isset($reg_meesage)){echo $reg_message;};
+			?>
+		</p>
 		<div class="confirm_links">
 			<a href="index.php">Return to Talon Air Home</a>
 			<br>
