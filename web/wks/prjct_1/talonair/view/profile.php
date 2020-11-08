49a7a66ddcd4941fcf8d782aef891a98b0fb89de
Timothy Wells <mr.timothywells@gmail.com>
2020-11-08 15:47:29 -0700

    20.11.08.18

--- web/wks/prjct_1/talonair/view/profile.php
+++ web/wks/prjct_1/talonair/view/profile.php
@@ -5,11 +5,14 @@
             include 'view/login.php';
         exit;
         }
-    } else {
+    }
+
+    if(!(isset($_SERVER['loggedIn']))){
         echo 'we are in the profile 2';
         include 'view/login.php';
         exit;
     }
+
     $pagetitle = 'Talon Air, Inc. | Profile';
     $_SESSION['customerSessionData']['fname'];
 ?>
