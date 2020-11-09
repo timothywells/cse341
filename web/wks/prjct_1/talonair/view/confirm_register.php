796d20e8d8b94ee788db1540c5c849c1b90f0ddf
Timothy Wells <mr.timothywells@gmail.com>
2020-11-09 02:08:35 -0700

    20.11.09.10

--- web/wks/prjct_1/talonair/view/confirm_register.php
+++ web/wks/prjct_1/talonair/view/confirm_register.php
@@ -1,5 +1,8 @@
 <?php 
-    $pagetitle = 'Talon Air, Inc. | Confirm Registration';
+	
+	
+	
+	$pagetitle = 'Talon Air, Inc. | Confirm Registration';
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,7 +21,7 @@
 		<h3><?php if(isset($reg_message)){echo $reg_message;};?></h3>
 
 		<div class="confirm_links">
-			<a class="links" href="index.php">Return to Talon Air Home</a>
+			<a class="links" href="index.php">Return to Home</a>
 			<a class="links" href="index.php?action=profile">Go to Profile</a>
 			<a class="links" href="index.php?action=write_review">Write a Review</a>
 		</div>
