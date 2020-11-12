cbf18c4dc4442083a7d317904c0c9fbff3b8be63
Timothy Wells <mr.timothywells@gmail.com>
2020-11-12 03:00:19 -0700

    20.11.12.12

--- web/wks/prjct_1/talonair/index.php
+++ web/wks/prjct_1/talonair/index.php
@@ -48,7 +48,7 @@ switch ($action){
                 if($confirm_register){
                     $_SESSION['registered'] = true; //Only things changed
                     $reg_message = "Registration Successful";
-                    include 'index.php?action=confirm_register';
+                    include 'view/confirm_register.php';
                 } else {
                     $reg_message = "Registration was NOT Successful";
                     include 'view/register.php';
@@ -58,25 +58,29 @@ switch ($action){
 
         //Confirm Registreation
             case 'confirm_register':
-                if($_SESSION['registered'] == true){
-                    $customerData = get_cust_profile($customerId); //Refers to get_cust_profile in functions.php
-                    $_SESSION['customerid'] = $customerData['customerid'];
-                    $_SESSION['username'] = $customerData['username'];
-                    $_SESSION['email'] = $customerData['email'];
-                    $_SESSION['fname'] = $customerData['fname'];
-                    $_SESSION['lname'] = $customerData['lname'];
-                    $_SESSION['street_address'] = $customerData['street_address'];
-                    $_SESSION['c_city'] = $customerData['c_city'];
-                    $_SESSION['c_state'] = $customerData['c_state'];
-                    $_SESSION['zip'] = $customerData['zip'];
-                    $_SESSION['phone'] = $customerData['phone'];
-                    $_SESSION['clearance'] = $customerData['clearance'];
-                    $_SESSION['loggedIn'] = true;
-                    include 'view/confirm_register.php';
-                } else {
-                    $reg_message = "Registration was NOT Successful";
-                    include 'view/register.php';
-                }
+                
+                
+                
+                
+                // if($_SESSION['registered'] == true){
+                //     $customerData = get_cust_profile($customerId); //Refers to get_cust_profile in functions.php
+                //     $_SESSION['customerid'] = $customerData['customerid'];
+                //     $_SESSION['username'] = $customerData['username'];
+                //     $_SESSION['email'] = $customerData['email'];
+                //     $_SESSION['fname'] = $customerData['fname'];
+                //     $_SESSION['lname'] = $customerData['lname'];
+                //     $_SESSION['street_address'] = $customerData['street_address'];
+                //     $_SESSION['c_city'] = $customerData['c_city'];
+                //     $_SESSION['c_state'] = $customerData['c_state'];
+                //     $_SESSION['zip'] = $customerData['zip'];
+                //     $_SESSION['phone'] = $customerData['phone'];
+                //     $_SESSION['clearance'] = $customerData['clearance'];
+                //     $_SESSION['loggedIn'] = true;
+                //     include 'view/confirm_register.php';
+                // } else {
+                //     $reg_message = "Registration was NOT Successful";
+                //     include 'view/register.php';
+                // }
             break;
         //
 
