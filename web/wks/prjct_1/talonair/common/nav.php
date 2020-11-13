a566d8ed4a545b2c789ff0520333cc25378416df
Timothy Wells <mr.timothywells@gmail.com>
2020-11-13 04:57:01 -0700

    20.11.13.18

--- web/wks/prjct_1/talonair/common/nav.php
+++ web/wks/prjct_1/talonair/common/nav.php
@@ -1,5 +1,15 @@
 <?php
- loginLink();
+if($_SESSION['loggedIn'] == false || isset($_SESSION['loggedIn']) == false){
+    $loginLinks = "<a href=index.php?action=login>Log in or Register</a>";
+}
+if($_SESSION['loggedIn'] == true || isset($_SESSION['loggedIn']) == true){
+    $loginLinks = "<a href=index.php?action=profile>Profile/Logout</a>";
+    if($_SESSION['clearance'] == true || isset($_SESSION['clearance']) == true){
+        $loginLinks = "<a href=index.php?action=admin>Adminisrtator</a>";
+    } else {
+        $loginLinks = "<a href=index.php?action=login>Log in or Register</a>";   
+    }
+}
 ?>
 <ul>
     <li><a href="index.php">Home</a></li>
