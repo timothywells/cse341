607d57d1dddb7abca5d68d10fa12425cf0a40cda
Timothy Wells <mr.timothywells@gmail.com>
2020-11-13 06:15:19 -0700

    20.11.13.29

--- web/wks/prjct_1/talonair/view/admin.php
+++ web/wks/prjct_1/talonair/view/admin.php
@@ -51,7 +51,7 @@
                 </div>
         </div>
     <br>
-        <div class="formatted_view profile">
+        <div class="formatted_view adminprofile">
                 <h3 class="center">Messages</h3>
                     <?php if(isset($message_del_message)){echo "<h5>" . $message_del_message . "</h5>";}; ?>
                     <div class="messages">
