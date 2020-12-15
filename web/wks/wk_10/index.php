95934f2af207519dd245e764789e72ec5ebd804b
Timothy Wells <mr.timothywells@gmail.com>
2020-12-14 20:52:06 -0700

    20.12.15.01
    Milestone 1

--- web/wks/wk_10/index.php
+++ web/wks/wk_10/index.php
@@ -13,11 +13,32 @@
     </nav> 
     <main>
         <h1>Week 10 - Database Access</h1>
-            <h2>Team Assignment</h2>
-                <p>Repository - <a href="https://github.com/Blake-Collin/cs313-php-Channel1/tree/master/05Teach">Here</a></p>
-            <h2>Individual Assignment</h2>
-                <p>Talon Air Website - <a href="https://morning-inlet-48465.herokuapp.com/wks/prjct_1/talonair/">Here</a></p>
-                <p>Repository - <a href="https://github.com/timothywells/cse341/tree/master/web/wks/prjct_1/talonair">Here</a></p>
+           <h2>Milestone 1</h2>
+            
+            <h3>Project title / one sentence description:</h3>
+            <p>Google Book API access using a external web server.</p>
+
+            <h3>Copy and paste the URL for your site at Heroku:</h3>
+            <p><a href="https://floating-ravine-61841.herokuapp.com/">Book Finder App</a></p>
+
+            <h3>Copy and paste the URL for your source code repository (e.g., GitHub):</h3>
+            <p><a href="https://github.com/timothywells/nodejs_bookfinder">Github Repository</a></p>
+
+            <h3>Describe the component that you completed from end-to-end:</h3>
+            <p>I have the design implemented and can send requests to the Google Book API but cannot recieve the information to be processed</p>
+
+            <h3>Please select the category you feel best describes your assignment:</h3>
+            <p>1 - Some attempt was made</p>
+            <p>2 - Developing, but significantly deficient</p>
+            <p>3 - Slightly deficient, but still mostly adequate</p>
+            <p>4 - Meets requirements</p>
+            <p>5 - Shows creativity and excels above and beyond requirements</p>
+
+            <h3>Provide a brief justification (1-2 sentences) for selecting that category.</h3>
+            <p>Honestly I put crazy amounts of time into doing this app so far and it still short from what I expected.<p>
+
+            <h3>Please list any questions you have for the instructor regarding this assignment or this week's topic.</h3>
+            <p>Why is this class made to be so difficult that even after hundreds of hours of studying I feel like I haven't ctually learned anything?</p>
     </main>
 </div>
     <footer>
