2fb7f4176902b3e5b9ee9d4f5007fb110bf601b6
Timothy Wells <mr.timothywells@gmail.com>
2020-12-15 02:50:12 -0700

    20.12.15.09

--- web/wks/wk_3/prove/index.php
+++ web/wks/wk_3/prove/index.php
@@ -26,49 +26,50 @@ session_start();
         <div id="item_div">
             <form class="items" id="item1" action="#" onclick="return addToCart(this);">
                 <img src="images/products/bomb.png">
-                <input type="button" value="Add To CART" onclick="cart('item1')">
                 <p>Bomb</p>
                 <p>Price - $200</p>
+                <input type="button" value="Add To CART" onclick="cart('item1')">
                 <input type="hidden" id="item1_name" value="Bomb">
                 <input type="hidden" id="item1_price" value="$200">
+            </form>
         </div>
         <div class="items" id="item2">
             <img src="product2.jpg">
-            <input type="button" value="Add To CART" onclick="cart('item2')">
             <p>Trendy T-Shirt With Back Design</p>
             <p>Price - $105</p>
+            <input type="button" value="Add To CART" onclick="cart('item2')">
             <input type="hidden" id="item2_name" value="Trendy T-Shirt With Back Design">
             <input type="hidden" id="item2_price" value="$105">
         </div>
         <div class="items" id="item3">
             <img src="product3.jpg">
-            <input type="button" value="Add To CART" onclick="cart('item3')">
             <p>Two Color Half-Sleeves T-Shirt</p>
             <p>Price - $120</p>
+            <input type="button" value="Add To CART" onclick="cart('item3')">
             <input type="hidden" id="item3_name" value="Two Color Half-Sleeves T-Shirt">
             <input type="hidden" id="item3_price" value="$120">
         </div>
         <div class="items" id="item4">
             <img src="product4.jpg">
-            <input type="button" value="Add To CART" onclick="cart('item4')">
             <p>Stylish Grey Chinos For Mens</p>
             <p>Price - $140</p>
+            <input type="button" value="Add To CART" onclick="cart('item4')">
             <input type="hidden" id="item4_name" value="Stylish Grey Chinos For Mens">
             <input type="hidden" id="item4_price" value="$140">
         </div>
         <div class="items" id="item5">
             <img src="product5.jpg">
-            <input type="button" value="Add To CART" onclick="cart('item5')">
             <p>Comfortable Pants For Working</p>
             <p>Price - $130</p>
+            <input type="button" value="Add To CART" onclick="cart('item5')">
             <input type="hidden" id="item5_name" value="Comfortable Pants For Working">
             <input type="hidden" id="item5_price" value="$130">
         </div>
         <div class="items" id="item6">
             <img src="product6.jpg">
-            <input type="button" value="Add To CART" onclick="cart('item6')">
             <p>Slim Track Pant For Jogging</p>
             <p>Price - $90</p>
+            <input type="button" value="Add To CART" onclick="cart('item6')">
             <input type="hidden" id="item6_name" value="Slim Track Pant For Jogging">
             <input type="hidden" id="item6_price" value="$90">
         </div>
